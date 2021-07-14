60d34eb7999ae7c2e2bf2379cbdc95db5ebaa0b1
kaiete <kaiete@aragon-press.com>
2021-07-14 09:29:27 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,11 +3,20 @@ let previousHappiness
 let noSadness = false
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     noSadness = true
-        basic.showString("You gave food to Liam!")
+        basic.showString("You fed Liam!")
         if (happiness != 3) {
             happiness = happiness + 1
         } else {
             happiness = 0
+        }
+    noSadness = false
+        
+})
+input.onButtonPressed(Button.B, function on_button_pressed_a() {
+    noSadness = true
+        basic.showString("You gave water to Liam!")
+        if (happiness == 0) {
+            happiness = 2
         }
     noSadness = false
         
@@ -54,7 +63,7 @@ basic.forever(function on_forever() {
     }
     
 })
-loops.everyInterval(20000,function() {
+loops.everyInterval(1200000,function() {
     if (happiness != 1 && noSadness != true) {
         happiness = happiness - 1
     }
