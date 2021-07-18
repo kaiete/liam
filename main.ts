3dcb9044c94c583a8daa05efb16b481500241fde
kaiete <kaiete@aragon-press.com>
2021-07-18 09:00:18 +0100

    Add radio communication (because it's cool)

--- main.ts
+++ main.ts
@@ -1,70 +1,93 @@
-let happiness = 3
-let previousHappiness
+let y: Image = null
+let x = ""
 let noSadness = false
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
+let happiness = 0
+happiness = 3
+let previousHappiness
+input.onButtonPressed(Button.A, function () {
     noSadness = true
-        basic.showString("You fed Liam!")
-        if (happiness != 3) {
-            happiness = happiness + 1
-        } else {
-            happiness = 0
-        }
+    basic.showString("You fed Liam!")
+    if (happiness != 3) {
+        happiness = happiness + 1
+    } else {
+        happiness = 0
+    }
     noSadness = false
-        
 })
-input.onButtonPressed(Button.B, function on_button_pressed_a() {
+function showMood () {
+    if (happiness == 3) {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+    } else if (happiness == 2) {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . . . . .
+            # # # # #
+            . . . . .
+            `)
+    } else if (happiness == 1) {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+    } else if (happiness == 0) {
+        basic.showLeds(`
+            . # . # .
+            . . . . .
+            . # # # .
+            # . . . #
+            # # # # #
+            `)
+    }
+}
+input.onButtonPressed(Button.AB, function () {
+    radio.setGroup(178)
+    radio.sendString("init")
+})
+input.onButtonPressed(Button.B, function () {
     noSadness = true
-        basic.showString("You gave water to Liam!")
-        if (happiness == 0) {
-            happiness = 2
-        }
+    basic.showString("You gave Liam water!")
+    if (happiness == 0) {
+        happiness = 2
+    }
     noSadness = false
-        
 })
-input.onGesture(Gesture.Shake, function() {
+input.onGesture(Gesture.Shake, function () {
     previousHappiness = happiness
     happiness = 0
     basic.pause(2000)
     happiness = previousHappiness
 })
-basic.forever(function on_forever() {
-    if (happiness == 3) {
-        basic.showLeds(`
-            . # . # .
-                        . # . # .
-                        . . . . .
-                        # . . . #
-                        . # # # .
+
+basic.forever(function () {
+    radio.setGroup(178)
+    x = radio.receiveString()
+    y = images.createBigImage(`
+        . . # . . . . # . .
+        . # . . . . . . # .
+        # # # # . . # # # #
+        . # . . . . . . # .
+        . . # . . . . # . .
         `)
-    } else if (happiness == 2) {
-        basic.showLeds(`
-            . # . # .
-                        . # . # .
-                        . . . . .
-                        # # # # #
-                        . . . . .
-        `)
-    } else if (happiness == 1) {
-        basic.showLeds(`
-            . # . # .
-                        . # . # .
-                        . . . . .
-                        . # # # .
-                        # . . . #
-        `)
-    } else if (happiness == 0) {
-        basic.showLeds(`
-            . # . # .
-                        . . . . .
-                        . # # # .
-                        # . . . #
-                        # # # # #
-        `)
+    if (x == "init") {
+        y.scrollImage(2, 200)
     }
-    
 })
-loops.everyInterval(1200000,function() {
+basic.forever(function () {
+    showMood()
+})
+loops.everyInterval(1200000, function () {
     if (happiness != 1 && noSadness != true) {
         happiness = happiness - 1
+        showMood()
     }
 })
